742c892e58f98767c2ee49b8a5abaff14ef17f85
wezaball <werachai0701@gmail.com>
2018-01-19 16:50:24 +0700

    my comment

---
+++ homework2/src/app/issue2/issue2.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-issue2',
+  templateUrl: './issue2.component.html',
+  styleUrls: ['./issue2.component.css']
+})
+export class Issue2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
